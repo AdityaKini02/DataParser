2ec600eba79b68499ce8b88780805bfc45bc7c96
adityakini <adityakemail@gmail.com>
2019-03-03 20:45:47 -0800

    test parse2016ElectionResults method

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /***
  * Main class for data parsers
  * @author Aditya Kini
@@ -8,5 +10,9 @@ public class Main {
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
         System.out.println(data);
+
+        ArrayList<ElectionResult> results = Utils.parse2016ElectionResults(data);
+
+
     }
 }
