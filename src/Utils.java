3cbb7b05a0452f21b8b4d6cd03af3d66a0209f19
adityakini <adityakemail@gmail.com>
2019-03-03 20:44:59 -0800

    parse2016ElectionResults method created

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -18,5 +19,57 @@ public class Utils {
         }
 
         return output.toString();
+    }
+
+    public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
+        ArrayList<ElectionResult> electionResults = new ArrayList<>();
+
+        //loop over data, make each row into it's own index in an arrayList
+        //take out each row seperatly
+        //remove the commas, and create a new Election Result object
+        //store it into electionResults
+
+        String d = normalizeLineBreaks(readFileAsString(data));
+        String[] lines = d.split("\n");
+
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+
+            for (int j = 0; j < line.length(); j++) {
+                String character = line.substring(j, j + 1);
+                if (character.equals("%")){
+                    character.replace("%", "");
+                }
+            }
+
+            String[] info = line.split(",");
+            double  votes_dem = Double.parseDouble(info[0]);
+            double votes_gop = Double.parseDouble(info[1]);
+            double total_votes = Double.parseDouble(info[2]);
+            double per_dem = Double.parseDouble(info[3]);
+            double per_gop = Double.parseDouble(info[4]);
+            int diff = Integer.parseInt(info[5]);
+            double per_point_diff = Double.parseDouble(info[6]);
+            String state_abbr = info[7];
+            String county_name = info[8];
+            int combined_fips = Integer.parseInt(info[10]);
+
+            ElectionResult e = new ElectionResult(votes_dem,votes_gop,total_votes,per_dem,per_gop,diff,per_point_diff,state_abbr,county_name,combined_fips);
+            electionResults.add(e);
+
+        }
+
+        return electionResults;
+
+
+    }
+
+    private static String normalizeLineBreaks(String s) {
+        s= s.replace('\u00A0',' '); // remove non-breaking whitespace characters
+        s= s.replace('\u2007',' ');
+        s= s.replace('\u202F',' ');
+        s= s.replace('\uFEFF',' ');
+
+        return s.replace("\r\n", "\n").replace('\r', '\n');
     }
 }
