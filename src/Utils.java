0a27781341b1d10c8515a83e120a3c11b164179e
adityakini <adityakemail@gmail.com>
2019-03-10 11:49:49 -0700

    edited parse2016ElectionResults method

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,83 +24,51 @@ public class Utils {
 
     public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
         ArrayList<ElectionResult> electionResults = new ArrayList<>();
-
-        //loop over data, make each row into it's own index in an arrayList
-        //take out each row seperatly
-        //remove the commas, and create a new Election Result object
-        //store it into electionResults
-
         String[] lines = readFileAsString(data).split("\n");
 
         for (int i = 0; i < lines.length; i++) {
             String line = lines[i];
-            String[] info = line.split(",");
+            int startIndex = line.indexOf(",") + 1;
 
-            for (int j = 1; j < info.length ; j++) {
-                String current = info[j];
+            line = line.substring(startIndex);
 
-                if (current.contains("\"")){
-                    current = removeCharacter(current, ",");
-                    current = removeCharacter(current, "\"");
-                }
-                if (current.contains("%")){
-                    current = removeCharacter(current, "%");
-                }
+            int indexOfFirstQuotation = line.indexOf("\"");
+            int indexOfLastQuotation = line.lastIndexOf("\"");
+            String[] info = null;
+
+            for (int j = 0; j < line.length(); j++) {
+                line = removeCharacter(line, "%");
+                line = removeCharacter(line, "," , indexOfFirstQuotation,indexOfLastQuotation);
+                line = removeCharacter(line, "\"");
+                info = line.split(",");
+
             }
 
-            double  votes_dem = Double.parseDouble(info[1]);
-            double votes_gop = Double.parseDouble(info[2]);
-            double total_votes = Double.parseDouble(info[3]);
-            double per_dem = Double.parseDouble(info[4]);
-            double per_gop = Double.parseDouble(info[5]);
-            int diff = Integer.parseInt(info[6]);
-            double per_point_diff = Double.parseDouble(info[7]);
-            String state_abbr = info[8];
-            String county_name = info[9];
-            int combined_fips = Integer.parseInt(info[10]);
+            double  votes_dem = Double.parseDouble(info[0]);
+            double votes_gop = Double.parseDouble(info[1]);
+            double total_votes = Double.parseDouble(info[2]);
+            double per_dem = Double.parseDouble(info[3]);
+            double per_gop = Double.parseDouble(info[4]);
+            int diff = Integer.parseInt(info[5]);
+            double per_point_diff = Double.parseDouble(info[6]);
+            String state_abbr = info[7];
+            String county_name = info[8];
+            int combined_fips = Integer.parseInt(info[9]);
 
             ElectionResult e = new ElectionResult(votes_dem,votes_gop,total_votes,per_dem,per_gop,diff,per_point_diff,state_abbr,county_name,combined_fips);
             electionResults.add(e);
+
+
         }
 
         return electionResults;
 
-
-//        for (int i = 0; i < lines.length; i++) {
-//            String line = lines[i].substring(2);
-//
-//            int startIndex = line.indexOf("\"");
-//            int endIndex = line.indexOf("\"", startIndex  + 1);
-//
-//            removeCharacter(line, "," , startIndex, endIndex);
-//            removeCharacter(line, "%");
-//            removeCharacter(line, "\"");
-//
-//
-//            String[] info = line.split(",");
-//            double  votes_dem = Double.parseDouble(info[0]);
-//            double votes_gop = Double.parseDouble(info[1]);
-//            double total_votes = Double.parseDouble(info[2]);
-//            double per_dem = Double.parseDouble(info[3]);
-//            double per_gop = Double.parseDouble(info[4]);
-//            int diff = Integer.parseInt(info[5]);
-//            double per_point_diff = Double.parseDouble(info[6]);
-//            String state_abbr = info[7];
-//            String county_name = info[8];
-//            int combined_fips = Integer.parseInt(info[9]);
-//
-//            ElectionResult e = new ElectionResult(votes_dem,votes_gop,total_votes,per_dem,per_gop,diff,per_point_diff,state_abbr,county_name,combined_fips);
-//            electionResults.add(e);
-//
-//        }
-//        return electionResults;
-
-
     }
+
 
     private static String removeCharacter(String line, String s){
         String newString = "";
-        for (int i = 0; i < line.length() - 1; i++) {
+        for (int i = 0; i < line.length(); i++) {
             if (! line.substring(i, i+1).equals(s)){
                 newString += line.substring(i, i+1);
             }
@@ -113,9 +82,9 @@ public class Utils {
         String newString = "";
 
         if(line.contains(s)) {
-            String val = line.substring(startIndex + 1, endIndex);
+            String val = line.substring(startIndex, endIndex+1);
 
-            for (int i = 0; i < val.length() - 1; i++) {
+            for (int i = 0; i < val.length(); i++) {
                 if (! val.substring(i, i+1).equals(s)){
                     newString += val.substring(i, i+1);
                 }
@@ -124,6 +93,4 @@ public class Utils {
         }
         return newString;
     }
-
-
 }
